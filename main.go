81ec5a10d6d894a3ee907becdcf6cb2ac7aaa551
Mark <m.rybalko@offsidegaming.com>
2026-05-18 12:24:21 +0200

    number validation and concurrency for TCP connections

--- main.go
+++ main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"log/slog"
+	"math"
 	"net"
 )
+
+type ExpectedRequest struct {
+	Method string  `json:"method"`
+	Number float64 `json:"number"`
+}
+
+type Resp struct {
+	Method string `json:"method"`
+	Prime  bool   `json:"prime"`
+}
 
 func main() {
 	l, err := net.Listen("tcp", ":8090")
@@ -30,13 +43,68 @@ func main() {
 
 func handleConnection(conn net.Conn) {
 	buffer := bufio.NewReader(conn)
+	malformedResponse, err := json.Marshal("malformed")
+	if err != nil {
+		slog.Error("marshaling malformed response", "error", err)
+	}
 
 	for {
-		response, err := buffer.ReadString('\n')
+		request, err := buffer.ReadString('\n')
 		if err != nil {
+			if err == io.EOF {
+				return
+			}
 			slog.Error("reading from buffer: %w", "error", err)
+			return
 		}
 
-		fmt.Fprintf(conn, "Echo: %s", response)
+		res, err := validateJson(request)
+		if err != nil {
+			conn.Write(malformedResponse)
+			return
+		}
+
+		resp, _ := json.Marshal(Resp{Method: "isPrime", Prime: isPrime(res.Number)})
+
+		conn.Write(resp)
 	}
+}
+
+func validateJson(requestString string) (ExpectedRequest, error) {
+	obj := ExpectedRequest{}
+	if err := json.Unmarshal([]byte(requestString), &obj); err != nil {
+		slog.Error("error parsing request: %v", "error", err)
+		return ExpectedRequest{}, fmt.Errorf("validating json: %w", err)
+	}
+
+	return obj, nil
+}
+
+func isPrime(num float64) bool {
+	if num != math.Trunc(num) {
+		return false
+	}
+
+	x := int64(num)
+
+	if x < 2 {
+		return false
+	}
+
+	if x == 2 || x == 3 {
+		return true
+	}
+
+	if x%2 == 0 {
+		return false
+	}
+
+	limit := int64(math.Sqrt(float64(x)))
+	for d := int64(3); d <= limit; d += 2 {
+		if x%d == 0 {
+			return false
+		}
+	}
+
+	return true
 }
